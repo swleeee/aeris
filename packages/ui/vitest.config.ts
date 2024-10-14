47964ed5e42852b415bbd66357ad2e521cbc77e1
SangWonLee <tkddnjs0687@naver.com>
2024-10-14 23:53:09 +0900

    chore: Update package and modify vitest.config

--- packages/ui/vitest.config.ts
+++ packages/ui/vitest.config.ts
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './setupTests.ts',
+    environment: 'jsdom', // NOTE: React 컴포넌트 테스트를 위해 JSDOM 환경 설정
+    globals: true, // NOTE: Vitest의 전역 매처를 활성화
   },
 })

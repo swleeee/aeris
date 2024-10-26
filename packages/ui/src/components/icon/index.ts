8bef597dc89ba5052a111604460ff1dc59f710a9
SangWonLee <tkddnjs0687@naver.com>
2024-10-26 19:16:13 +0900

    feat: Add some icon component to ui package

---
+++ packages/ui/src/components/icon/index.ts
@@ -0,0 +1,4 @@
+export { default as SelectedHeartIcon } from './SelectedHeartIcon'
+export { default as SelectedStarIcon } from './SelectedStarIcon'
+export { default as UnselectedHeartIcon } from './UnselectedHeartIcon'
+export { default as UnselectedStarIcon } from './UnselectedStarIcon'

4b2b66f84b75f37f73a86481e6fbe044c8fab0b7
SangWonLee <tkddnjs0687@naver.com>
2024-11-09 21:53:13 +0900

    storybook: Add storybook for checkbox group component

--- packages/ui/src/components/checkbox/Checkbox.stories.tsx
+++ packages/ui/src/components/checkbox/Checkbox.stories.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Meta, StoryObj } from '@storybook/react'
 
 import Checkbox from './Checkbox'
+import CheckboxGroup from './CheckboxGroup'
 import { SelectedStarIcon, UnselectedStarIcon } from '../icon'
 import { colorPalette } from '../../styles/colors'
 
@@ -24,9 +25,10 @@ const checkboxStateDecorator = (Story, context) => {
   return <Story {...context} args={args} />
 }
 
-const meta: Meta<typeof Checkbox> = {
+const meta: Meta<typeof Checkbox | typeof CheckboxGroup> = {
   title: 'Components/Checkbox',
   component: Checkbox,
+  subcomponents: { CheckboxGroup },
   decorators: [checkboxStateDecorator],
   argTypes: {
     size: {
@@ -51,6 +53,13 @@ const meta: Meta<typeof Checkbox> = {
     checkedIcon: {
       options: ['filledStar', 'filledCircle'],
       control: { type: 'radio' },
+    },
+    direction: {
+      options: ['horizontal', 'vertical'],
+      control: { type: 'radio' },
+      defaultValue: 'vertical',
+      description: 'CheckboxGroup direction',
+      table: { category: 'CheckboxGroup' },
     },
   },
 }
@@ -157,5 +166,47 @@ export const WithCustomIcons: Story = {
         checkedIconOptions[args.checkedIcon as keyof typeof checkedIconOptions]
       }
     />
+  ),
+}
+
+type CheckboxGroupStory = StoryObj<typeof CheckboxGroup>
+
+export const DefaultCheckboxGroup: CheckboxGroupStory = {
+  args: {
+    options: [
+      { label: 'Option 1', value: 'option1' },
+      { label: 'Option 2', value: 'option2' },
+      { label: 'Option 3', value: 'option3' },
+    ],
+    direction: 'vertical',
+  },
+  render: (args) => (
+    <CheckboxGroup {...args} onChange={(values) => console.log(values)} />
+  ),
+}
+
+export const HorizontalCheckboxGroup: CheckboxGroupStory = {
+  args: { ...DefaultCheckboxGroup.args, direction: 'horizontal' },
+  render: (args) => <CheckboxGroup {...args} />,
+}
+
+export const DisabledCheckboxGroup: CheckboxGroupStory = {
+  args: { ...DefaultCheckboxGroup.args, disabled: true },
+  render: (args) => <CheckboxGroup {...args} />,
+}
+
+export const PreselectedCheckboxGroup: CheckboxGroupStory = {
+  args: { ...DefaultCheckboxGroup.args, value: ['option1', 'option3'] },
+  render: (args) => <CheckboxGroup {...args} />,
+}
+
+export const CheckboxGroupWithCustomSize: CheckboxGroupStory = {
+  args: { ...DefaultCheckboxGroup.args },
+  render: (args) => (
+    <div style={{ display: 'flex', flexDirection: 'column', rowGap: '1rem' }}>
+      <CheckboxGroup {...args} size="sm" />
+      <CheckboxGroup {...args} size="md" />
+      <CheckboxGroup {...args} size="lg" />
+    </div>
   ),
 }
